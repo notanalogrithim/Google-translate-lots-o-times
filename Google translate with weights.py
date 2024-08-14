234beec524bdbf2abe86b78d5e3d63a761a8695f
Not an algorithm <kai@devcoder.net>
2024-08-14 12:33:09 +1000

    Update Google translate with weights.py
    I tried to kill it but I saved it

--- Google translate with weights.py
+++ Google translate with weights.py
@@ -3,8 +3,6 @@ import time
 import random
 from deep_translator import GoogleTranslator
 
-#Does this even work, is it even needed, who knows
-random.seed(random.randint)
 tenComandments = """
 1. לֹא יִהְיֶה לְךָ אֱלֹהִים אֲחֵרִים עַל פָּנָי.
 2. לֹא תַעֲשֶׂה לְךָ פֶּסֶל וְכָל תְּמוּנָה.
